654be71a1381d41d1cf70dbb02e0079a4211a2de
RaisRichard <richard@rais.page>
2024-04-21 17:15:27 +0200

    Almost all sections done, new design

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as InfoDisplay } from "./InfoDisplay.astro";
 export { default as Section } from "./Section.astro";
 export { default as ListSection } from "./ListSection.astro";
 export { default as ListSectionEntry } from "./ListSectionEntry.astro";
+export { default as LanguageSection } from "./LanguagesSection.astro";
+export { default as SkillsSection } from "./SkillsSection.astro";
+export { default as InterestsSection } from "./InterestsSection.astro";
--- src/util/translations.ts
+++ src/util/translations.ts
@@ -1,19 +1,28 @@
 interface Translations {
     professionalExperience: string;
     education: string;
+    languages: string;
+    skills: string;
+    interests: string;
     present: string;
 }
 
 const translationsDE: Translations = {
     professionalExperience: "Berufliche Erfahrung",
     education: "Schulbildung",
-    present: "Heute"
+    languages: "Sprachkenntnisse",
+    skills: "Kenntnisse",
+    interests: "Interessen / Hobbys",
+    present: "Heute",
 }
 
 const translationsEN: Translations = {
     professionalExperience: "Professional Experience",
     education: "Education",
-    present: "Present"
+    languages: "Languages",
+    skills: "Skills",
+    interests: "Interests / Hobbies",
+    present: "Present",
 }
 
 export const translations = {
