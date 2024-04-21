156a586e5a2fe323946c692ed9d850f934f87a8e
RaisRichard <richard@rais.page>
2024-04-21 12:35:45 +0200

    First few astro components and pages

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as InfoDisplay } from "./InfoDisplay.astro";
+export { default as Section } from "./Section.astro";
+export { default as ListSection } from "./ListSection.astro";
+export { default as ListSectionEntry } from "./ListSectionEntry.astro";
